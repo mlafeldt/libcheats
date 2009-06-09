ffd284a3f5f94e43a89c19cd0c64790d470e20a0
Mathias Lafeldt <misfire@debugon.org>
2009-06-09 14:31:40 +0200

    Implement sample to read codes from text file.

--- src/main.c
+++ src/main.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 #include "mytypes.h"
+#include "codeman.h"
 
 int main(int argc, char *argv[])
 {
+	gamelist_t list;
+
+	cl_init(&list);
+
+	codes_from_textfile(argv[1], &list, CODES_OPT_GID);
+
+	cl_print(&list);
+	cl_free(&list);
+
 	return 0;
 }
