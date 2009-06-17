26f9180564adab65a859d7cc7c60d23118c15fcf
Mathias Lafeldt <misfire@debugon.org>
2009-06-17 19:42:40 +0200

    Remove dllist.c.

--- src/dllist.h
+++ src/dllist.h
@@ -24,6 +24,8 @@
 #ifndef _DLLIST_H_
 #define _DLLIST_H_
 
+#include <stdlib.h> /* for NULL and free() */
+
 /**
  * node_t - node prototype
  * @next: ptr to next node in list
@@ -48,12 +50,120 @@ typedef struct _list {
 	void *tail;
 } list_t;
 
-void list_init(void *list);
-int list_is_empty(const void *list);
-void list_add(void *list, void *node);
-void list_insert(void *list, void *node, void *after);
-void *list_remove(void *list, void *node);
-void list_clear(void *list);
-void list_free(void *list);
+
+/**
+ * list_init - Initialize the list.
+ * @list: list to be initialized
+ */
+static inline void list_init(void *list)
+{
+	list_t *l = (list_t*)list;
+
+	l->head = NULL;
+	l->tail = NULL;
+}
+
+/**
+ * list_is_empty - Check if the list is empty.
+ * @list: list
+ * @return: non-zero if list is emtpy
+ */
+static inline int list_is_empty(const void *list)
+{
+	return ((list_t*)list)->head == NULL;
+}
+
+/**
+ * list_add - Add a node to the end of the list.
+ * @list: list
+ * @node: node to be added
+ */
+static inline void list_add(void *list, void *node)
+{
+	list_t *l = (list_t*)list;
+	node_t *n = (node_t*)node;
+
+	if (l->head == NULL) {
+		l->head = n;
+		n->prev = NULL;
+	} else {
+		((node_t*)l->tail)->next = n;
+		n->prev = l->tail;
+	}
+
+	l->tail = n;
+	n->next = NULL;
+}
+
+/**
+ * list_insert - Insert a node after another node.
+ * @list: list
+ * @node: node to be inserted
+ * @after: node after which to insert
+ */
+static inline void list_insert(void *list, void *node, void *after)
+{
+	list_t *l = (list_t*)list;
+	node_t *n = (node_t*)node;
+	node_t *a = (node_t*)after;
+
+	n->next = a->next;
+	n->prev = a;
+
+	if (a->next != NULL)
+		a->next->prev = n;
+	else
+		l->tail = n;
+
+	a->next = n;
+}
+
+/**
+ * list_remove - Remove a node from the list.
+ * @list: list
+ * @node: node to be removed
+ * @return: ptr to removed node
+ */
+static inline void *list_remove(void *list, void *node)
+{
+	list_t *l = (list_t*)list;
+	node_t *n = (node_t*)node;
+
+	if (n->prev == NULL)
+		l->head = n->next;
+	else
+		n->prev->next = n->next;
+
+	if (n->next == NULL)
+		l->tail = n->prev;
+	else
+		n->next->prev = n->prev;
+
+	return node;
+}
+
+/**
+ * list_clear - Clear the list by removing all nodes.
+ * @list: list to be cleared
+ */
+static inline void list_clear(void *list)
+{
+	list_t *l = (list_t*)list;
+
+	while (l->head != NULL)
+		list_remove(l, l->head);
+}
+
+/**
+ * list_free - Remove all nodes from the list and deallocate the used memory.
+ * @list: list to be freed
+ */
+static inline void list_free(void *list)
+{
+	list_t *l = (list_t*)list;
+
+	while (l->head != NULL)
+		free(list_remove(l, l->head));
+}
 
 #endif /* _DLLIST_H_ */
