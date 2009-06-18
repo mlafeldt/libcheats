1ca4f48fd0aba892526d7084650e6f34658a3bf5
Mathias Lafeldt <misfire@debugon.org>
2009-06-18 13:12:34 +0200

    Add another test case.

--- src/test1.c
+++ src/test1.c
@@ -1,37 +1,23 @@
-#include <stdio.h>
-#include "mytypes.h"
-#include "libcheats.h"
+/*
+ * test cheats_read_file()
+ */
 
-static const char *text =
-"\"TimeSplitters 2 Demo PAL\"\n"
-"Mastercode\n"
-"F02000A4 0000000E\n"
-"Inf. Schnubbel\n"
-"0021FF44 00000002\n"
-"\"Tom Clancy's Splinter Cell PAL\"\n"
-"Mastercode\n"
-"90129A84 0C048534\n"
-"Unlock all Levels\n"
-"20383A30 00000000\n";
+#include <stdio.h>
+#include "libcheats.h"
 
 int main(int argc, char *argv[])
 {
 	cheats_t cheats;
-	int ret;
 
 	cheats_init(&cheats);
 
-//	if (cheats_read_file(&cheats, argv[1]) != CHEATS_TRUE) {
-//		printf("line: %i\nerror: %s\n", cheats.error_line, cheats.error_text);
-//		return -1;
-//	}
+	if (cheats_read_file(&cheats, argv[1]) != CHEATS_TRUE) {
+		printf("line: %i\nerror: %s\n",
+			cheats.error_line, cheats.error_text);
+		cheats_destroy(&cheats);
+		return -1;
+	}
 
-	ret = cheats_read_buf(&cheats, text);
-//	cheats_read(&cheats, stdin);
-
-//	cheats_write_file(&cheats, "out");
-//	cheats_write(&cheats, stdout);
 	cheats_destroy(&cheats);
-
-	return ret;
+	return 0;
 }
