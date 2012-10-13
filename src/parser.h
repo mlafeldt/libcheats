e8cba95a046cc3a13f764274ce013c3b93e7aa99
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2012-10-13 20:06:24 +0200

    Replace GPL header with MIT header in source files

--- src/parser.h
+++ src/parser.h
@@ -1,22 +1,11 @@
 /*
  * parser.h - Parse cheats in text format
  *
- * Copyright (C) 2009 misfire <misfire@xploderfreax.de>
+ * Copyright (C) 2009-2012 Mathias Lafeldt <mathias.lafeldt@gmail.com>
  *
  * This file is part of libcheats.
  *
- * libcheats is free software: you can redistribute it and/or modify it under
- * the terms of the GNU Lesser General Public License as published by the Free
- * Software Foundation, either version 3 of the License, or (at your option) any
- * later version.
- *
- * libcheats is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
- * A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
- * details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with libcheats.  If not, see <http://www.gnu.org/licenses/>.
+ * libcheats is licensed under the terms of the MIT License. See LICENSE file.
  */
 
 #ifndef _PARSER_H_
